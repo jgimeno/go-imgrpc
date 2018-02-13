1bf98da12fc81e9638238e5429379b60a441fcb8
Jonathan Gimeno <jgimeno@gmail.com>
2018-02-13 23:39:38 +0100

    Now it gets the id when saving with the client

--- client/main.go
+++ client/main.go
@@ -5,22 +5,63 @@ import (
 	"github.com/jgimeno/go-imgrpc/protocolbuffer"
 	"context"
 	"io/ioutil"
+	"github.com/urfave/cli"
+	"os"
+	"github.com/pkg/errors"
+	"fmt"
+)
+
+var (
+	ErrMissingFileName = errors.New("Missing image filename.")
 )
 
 func main() {
-	conn, err := grpc.Dial(":8080", grpc.WithInsecure())
-	if err != nil {
-		panic("Error connecting to server." + err.Error())
+	app := cli.NewApp()
+
+	app.Commands = []cli.Command{
+		{
+			Name: "save",
+			Aliases: []string{"s"},
+			Usage: "Save an image in the microservice",
+			Action: func(c *cli.Context) error {
+				fileName := c.Args().First()
+
+				if len(c.Args()) < 1 {
+					return ErrMissingFileName
+				}
+
+				conn, err := grpc.Dial(":8080", grpc.WithInsecure())
+				if err != nil {
+					return ErrMissingFileName
+				}
+
+				defer conn.Close()
+				client := protocolbuffer.NewImageServiceClient(conn)
+
+				imgBytes, _ := ioutil.ReadFile(fileName)
+
+				img := &protocolbuffer.Image{
+					Data: imgBytes,
+				}
+
+				id, _ := client.SaveImage(context.Background(), img)
+				fmt.Printf(id.Id)
+				return nil
+			},
+		},
+		{
+			Name: "get",
+			Aliases: []string{"g"},
+			Usage: "Use to get the files by id.",
+			Action: func(c *cli.Context) error {
+				fmt.Print("Getting the file.")
+				return nil
+			},
+		},
 	}
 
-	defer conn.Close()
-	client := protocolbuffer.NewImageServiceClient(conn)
-
-	imgBytes, _ := ioutil.ReadFile("gopher.jpg")
-
-	img := &protocolbuffer.Image{
-		Data: imgBytes,
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println("Error: " + err.Error())
+		os.Exit(1)
 	}
-
-	client.SaveImage(context.Background(), img)
 }
